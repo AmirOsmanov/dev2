a9b1a6cbd3996e00adb094b6e8f9546e4b03fbc9
Amir Osmanov <amirosmanov007@gmail.com>
2025-04-02 18:24:40 +0300

    Second commit

--- main.go
+++ main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"time"
 )
 
 func main() {
 	s := make([]string, 0)
+	results := make([]map[string]string, 0)
 	s = append(s, "http://www.testingmcafeesites.com/index.html")
-	s = append(s, "http://www.testingmcafeesites.com/testcat_ac.html")
+	s = append(s, "http://www.neverssl.com/")
 	s = append(s, "http://www.testingmcafeesites.com/testcat_al.html")
 	s = append(s, "http://www.testingmcafeesites.com/testcat_an.html")
 	s = append(s, "http://www.testingmcafeesites.com/testcat_au.html")
 	s = append(s, "http://www.testingmcafeesites.com/testcat_be.html")
 
 	for _, value := range s {
+		start := time.Now()
 		res, err := http.Get(value)
+		delta := time.Since(start)
+
+		result := make(map[string]string)
+		result["url"] = value
+		result["status"] = fmt.Sprintf("%d", res.StatusCode)
+		result["time"] = delta.String()
+
 		if err != nil {
-			log.Printf("error making http request: %s\n", err)
-			os.Exit(1)
+			result["error"] = err.Error()
+			log.Fatalf("error: %s url: %s\n", err, value)
 		}
-		log.Print(res.StatusCode)
+
+		results = append(results, result)
+
+		defer res.Body.Close()
+	}
+	for _, r := range results {
+		log.Println(r)
 	}
 }
